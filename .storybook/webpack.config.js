62aae40ca644372b490adef94ee117b56b16fc82
S. M. Catala <stephane@zenyway.com>
2018-10-08 11:14:47 +0200

    support sourcemaps in storybook & svg imports

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,6 +2,12 @@
 const path = require('path')
 
 module.exports = (baseConfig, env, config) => {
+  // svg loader: run first, before default svg loader (= file-loader)
+  config.module.rules.unshift({
+    test: /\.svg$/,
+    loader: 'transform-loader?imgurify'
+  })
+
   // typescript
   const tsconfig = path.resolve(__dirname, '../src/tsconfig.json')
 
@@ -15,12 +21,17 @@ module.exports = (baseConfig, env, config) => {
       path.resolve(__dirname, '../src'),
       path.resolve(__dirname, '../stubs'),
     ],
-    loader: require.resolve('ts-loader'),
+    loader: 'ts-loader',
     options: {
       configFile: tsconfig,
-      logLevel: 'info'
+      logLevel: 'info',
+      compilerOptions: {
+        "sourceMap": true // https://github.com/TypeStrong/ts-loader#devtool--sourcemaps
+      }
     }
   })
+
+  config.devtool = 'inline-source-map' // development only
 
   // add alias specs from package browser field
   config.resolve.aliasFields = (config.resolve.aliasFields || []).concat(
